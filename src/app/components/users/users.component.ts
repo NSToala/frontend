92b5b080571b0dcbf92859e44814c825df01d904
Edgar Ramirez <edgartoala@gmail.com>
2022-03-05 03:04:11 -0600

    fix users

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
 import { AuthService } from '../../services/auth.service'
 import {Router} from "@angular/router"
 
@@ -19,7 +18,6 @@ export class UsersComponent implements OnInit {
   verifyPermission() {
     this.authService.getUsers(localStorage.getItem('token')!).subscribe(
       (res: any) => {
-        console.log(res)
         this.authService.Users = res
         //this.router.navigate(['/users'])
       },
